6ba83f2458845cafbd862aad4ecc27e489aab323
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2022-01-06 11:43:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function randomValueFromArray(array){
 const storyText = 'その日は、とても暑かった。 そして :insertx:は出かけた. :inserty:に着いた。そして彼は :insertZ。 ボブは一部始終を見ていたが、全く驚かなかった。 — :insertx:はドーナツが好物だった。 それは暑い日だった。';
 const insertX = ['ミッキー', 'スポンジボブ', 'ドナルド'];
 const insertY = ['ディズニーシー', '金閣寺', '大涌谷'];
-const insertZ = ['大喜びした', '軽快にダンスした', '飲み物を一口飲んだ'];
+const insertZ = ['混雑していた', '雪が降っていた', '鳥が多かった'];
 
 randomize.addEventListener('click', result);
 
